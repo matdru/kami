7fd2fa7ca29c57b1f1f55917e74c4e690c375b4c
Mateusz Drulis <mateusz.drulis@gmail.com>
2019-04-20 10:29:29 +0100

    Updates

---
+++ precache-manifest.f637a8c51f5dd0086f593303588e818e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9118fa821f62ed2b541e",
+    "url": "/slacker/static/js/runtime~main.86673fd6.js"
+  },
+  {
+    "revision": "31a849381ef2464e4d62",
+    "url": "/slacker/static/js/main.8cdc15bf.chunk.js"
+  },
+  {
+    "revision": "a718af4ab862df1526e3",
+    "url": "/slacker/static/js/2.1ba97405.chunk.js"
+  },
+  {
+    "revision": "31a849381ef2464e4d62",
+    "url": "/slacker/static/css/main.65f34b2b.chunk.css"
+  },
+  {
+    "revision": "a718af4ab862df1526e3",
+    "url": "/slacker/static/css/2.168cdb51.chunk.css"
+  },
+  {
+    "revision": "92c03b6d0d5972bf458ed5006301900f",
+    "url": "/slacker/index.html"
+  }
+];
