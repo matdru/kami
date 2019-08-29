7bea22b6899bf14d25081e673e7ca6fa1c07cd47
Mateusz Drulis <mateusz.drulis@gmail.com>
2019-08-29 17:06:35 +0100

    Updates

---
+++ precache-manifest.882c57766973a1fe404e0ad101157b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320e60f770533231f6771d8557ed1d66",
+    "url": "/kami/index.html"
+  },
+  {
+    "revision": "2a1a1c9c726233a477e8",
+    "url": "/kami/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "73ef1cab04f4525ebd7d",
+    "url": "/kami/static/css/main.bcf5a46d.chunk.css"
+  },
+  {
+    "revision": "2a1a1c9c726233a477e8",
+    "url": "/kami/static/js/2.357c2bf0.chunk.js"
+  },
+  {
+    "revision": "73ef1cab04f4525ebd7d",
+    "url": "/kami/static/js/main.f898e0a4.chunk.js"
+  },
+  {
+    "revision": "771671db240c41179c03",
+    "url": "/kami/static/js/runtime~main.1007478c.js"
+  }
+]);
