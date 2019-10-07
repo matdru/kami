60022fdf454cf9b31ff4ee3dfed31309446f83d3
Mateusz Drulis <mateusz.drulis@gmail.com>
2019-10-07 01:30:37 +0100

    Updates

---
+++ precache-manifest.433b7a14daca0fa33851605da744f2eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7095a459cc2354f458b2baea223e1414",
+    "url": "/kami/index.html"
+  },
+  {
+    "revision": "7259d8547632f1ac997a",
+    "url": "/kami/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "f9c24418c90be92ca34d",
+    "url": "/kami/static/css/main.bcf5a46d.chunk.css"
+  },
+  {
+    "revision": "7259d8547632f1ac997a",
+    "url": "/kami/static/js/2.428bc626.chunk.js"
+  },
+  {
+    "revision": "f9c24418c90be92ca34d",
+    "url": "/kami/static/js/main.2974fb22.chunk.js"
+  },
+  {
+    "revision": "771671db240c41179c03",
+    "url": "/kami/static/js/runtime~main.1007478c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kami/precache-manifest.6b34f72547bab1510d85da7f639afaa2.js"
+  "/kami/precache-manifest.433b7a14daca0fa33851605da744f2eb.js"
 );
 
 self.addEventListener('message', (event) => {
